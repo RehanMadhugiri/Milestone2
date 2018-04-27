ffc62a847cd02967576857ece6c388e75d51c72e
nickmerf <37085457+nickmerf@users.noreply.github.com>
2018-04-26 19:08:30 -0500

    Updated Matchup.java

--- src/application/Matchup.java
+++ src/application/Matchup.java
@@ -1,11 +1,34 @@
 package application;
 
+import java.util.Random;
+
 public class Matchup {
 	
-	private Challenger c1;
-	private Challenger c2;
+	Challenger c1;
+	Challenger c2;
 	
+	public Matchup(Challenger c1, Challenger c2){
+		this.c1 = c1;
+		this.c2 = c2;
+	}
+	
+	// returns the winner of the game (challenger with greater score)
+	// if there is a tie, a winner will be picked randomly
 	public Challenger getWinner(Challenger c1, Challenger c2) {
-		return null;
+		// Tie 
+		if(c1.getScore() == c2.getScore()){
+			Random random = new Random(2);
+			int result = random.nextInt();
+			if(result == 0) return c1;
+			else return c2;
+		}
+		
+		// c1's score is greater
+		if(c1.getScore() > c2.getScore()){
+			return c1;
+		}
+		
+		// c2's score is greater
+		return c2;
 	}
 }
