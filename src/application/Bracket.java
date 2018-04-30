a71f66f5473e642afc0d4b23fa3265d9c6a1ade9
Andy Waldron <awaldron2@wisc.edu>
2018-04-29 21:56:37 -0500

    Deleted unnecessary print statement

--- src/application/Bracket.java
+++ src/application/Bracket.java
@@ -11,9 +11,6 @@ public class Bracket {
 	
 	public Bracket(ArrayList<Challenger> challengers) { // Or make constructor a static method and name it to generateBracket as originally planned in Milestone1.
 		activeChallengers = challengerSorter(challengers);
-		for(int i=0; i<activeChallengers.size(); i++) {
-			System.out.println(activeChallengers.get(i).getName());
-		}
 		challengersAtStart = new Challenger[challengers.size()];
 		for(int i=0; i<challengers.size(); i++) {
 			challengersAtStart[i] = activeChallengers.get(i);
