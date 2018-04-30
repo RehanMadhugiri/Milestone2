ce9879a362c344598bbbc8d08e65541df22258c1
Andy Waldron <awaldron2@wisc.edu>
2018-04-29 19:55:32 -0500

    Generalized stuff

--- src/application/Bracket.java
+++ src/application/Bracket.java
@@ -7,10 +7,13 @@ public class Bracket {
 	private Challenger[] challengersAtStart;
 	private ArrayList<Challenger> activeChallengers;
 	private int numChallengers;
-	private ArrayList<Matchup> matchups;
+	private ArrayList<Matchup> matchups = new ArrayList<Matchup>();
 	
 	public Bracket(ArrayList<Challenger> challengers) { // Or make constructor a static method and name it to generateBracket as originally planned in Milestone1.
-		activeChallengers = challengers;
+		activeChallengers = challengerSorter(challengers);
+		for(int i=0; i<activeChallengers.size(); i++) {
+			System.out.println(activeChallengers.get(i).getName());
+		}
 		challengersAtStart = new Challenger[challengers.size()];
 		for(int i=0; i<challengers.size(); i++) {
 			challengersAtStart[i] = activeChallengers.get(i);
@@ -28,6 +31,53 @@ public class Bracket {
 	
 	public void eliminateChallenger(Challenger c) {
 		activeChallengers.remove(c);
+	}
+	
+	public ArrayList<Matchup> getMatchups() {
+		return matchups;
+	}
+	
+	public ArrayList<Challenger> challengerSorter(ArrayList<Challenger> challengerList) {
+		ArrayList<Challenger> mergingArray = new ArrayList<Challenger>();
+		if(challengerList.size() == 2) {
+			return challengerList;
+		} else {
+			ArrayList<Challenger> half1 = new ArrayList<Challenger>();
+			ArrayList<Challenger> half2 = new ArrayList<Challenger>();
+			int incrementCount1 = 0;
+			int incrementCount2 = 1;
+			for(int i=0; i<challengerList.size(); i+=3) {
+				half1.add(challengerList.get(i));
+				if(incrementCount1 == 1) {
+					if(i != challengerList.size() - 1) {
+						half1.add(challengerList.get(i+1));
+						i++;
+					}
+				} else {
+					incrementCount1++;
+				}
+			}
+			for(int i=1; i<challengerList.size(); i+=3) {
+				half2.add(challengerList.get(i));
+				if(incrementCount2 == 1) {
+					if(i != challengerList.size() - 1) {
+						half2.add(challengerList.get(i+1));
+						i++;
+					}
+				} else {
+					incrementCount2++;
+				}
+			}
+			ArrayList<Challenger> subArray1 = challengerSorter(half1);
+			for(int i=0; i<subArray1.size(); i++) {
+				mergingArray.add(subArray1.get(i));
+			}
+			ArrayList<Challenger> subArray2 = challengerSorter(half2);
+			for(int i=0; i<subArray2.size(); i++) {
+				mergingArray.add(subArray2.get(i));
+			}
+			return mergingArray;
+		}
 	}
 	
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -22,6 +22,13 @@ public class Main extends Application {
 	
 	public static Bracket bracket;
 	public static ArrayList<Label> teamLabels;
+	private static ArrayList<ArrayList<TextField>> teamScores;
+	private static ArrayList<ArrayList<Button>> submitButtons;
+	private static ArrayList<ArrayList<Label>> TBDLabels;
+	private static int numSubmit;
+	private static int numTextFields;
+	private static int numTBDLabels;
+	private static int numRounds;
 	
 	@Override
 	public void start(Stage primaryStage) { // Needs to be changed to display who come in first, second, and third place.
@@ -32,10 +39,48 @@ public class Main extends Application {
 			root.setPadding(new Insets(10, 20, 10, 20));
 			root.setTop(new Label("Enter a score in the text fields and press submit when both scores are added."));
 			
-			int y_coord = 2;
-			for(int i=0; i<teamLabels.size(); i++) {
-				gridPane.add(teamLabels.get(i), 0, y_coord);
-				y_coord += 5;
+			int teamYCoord = 2;
+			for(int i=0; i<teamLabels.size(); i+=2) {
+				gridPane.add(teamLabels.get(i), 0, teamYCoord);
+				gridPane.add(teamLabels.get(i+1), 0, teamYCoord + 2);
+				teamYCoord += 5;
+			}
+			
+			int scoreXCoord = 1;
+			int scoreYCoord = 2;
+			int specialYCoord = 10;
+			int spaceBetweenChallengers = 20;
+			int spaceBetweenSubBrackets = 40;
+			for(int i=0; i<numRounds; i++) {
+				if(i==0) {
+					for(int j=0; j<teamScores.get(i).size(); j+=2) {
+						gridPane.add(teamScores.get(i).get(j), scoreXCoord, scoreYCoord);
+						gridPane.add(teamScores.get(i).get(j+1), scoreXCoord, scoreYCoord + 2);
+						scoreYCoord += 5;
+					}
+					scoreYCoord = 3;
+				} else if(i==1) {
+					for(int j=0; j<teamScores.get(i).size(); j+=2) {
+						gridPane.add(teamScores.get(i).get(j), scoreXCoord, scoreYCoord);
+						gridPane.add(teamScores.get(i).get(j+1), scoreXCoord, scoreYCoord + 5);
+						scoreYCoord += 10;
+					}
+					scoreYCoord = 5;
+				} else if(i==2) {
+					for(int j=0; j<teamScores.get(i).size(); j+=2) {
+						gridPane.add(teamScores.get(i).get(j), scoreXCoord, scoreYCoord);
+						gridPane.add(teamScores.get(i).get(j+1), scoreXCoord, scoreYCoord + 10);
+						scoreYCoord += 20;
+					}
+					scoreYCoord *= 2;
+				} else {
+					for(int j=0; j<teamScores.get(i).size(); j+=2) {
+						gridPane.add(teamScores.get(i).get(j), scoreXCoord, specialYCoord);
+						gridPane.add(teamScores.get(i).get(j+1), scoreXCoord, specialYCoord + 10);
+						scoreYCoord += 20;
+					}
+				}
+				scoreXCoord+=2;
 			}
 			
 //			Label team1 = new Label("Team 01");
@@ -61,8 +106,8 @@ public class Main extends Application {
 //			gridPane.add(team1, 0, 2);
 //			gridPane.add(team16, 0, 4);
 			gridPane.add(submit1_1, 0, 3);
-			gridPane.add(score1_1, 1, 2);
-			gridPane.add(score1_2, 1, 4);
+//			gridPane.add(score1_1, 1, 2);
+//			gridPane.add(score1_2, 1, 4);
 			
 			Button submit1_2 = new Button("Submit");
 			TextField score1_3 = new TextField("score...");
@@ -71,8 +116,8 @@ public class Main extends Application {
 //			gridPane.add(team2, 0, 7);
 //			gridPane.add(team15, 0, 9);
 			gridPane.add(submit1_2, 0, 8);
-			gridPane.add(score1_3, 1, 7);
-			gridPane.add(score1_4, 1, 9);
+//			gridPane.add(score1_3, 1, 7);
+//			gridPane.add(score1_4, 1, 9);
 			
 			Button submit1_3 = new Button("Submit");
 			TextField score1_5 = new TextField("score...");
@@ -82,8 +127,8 @@ public class Main extends Application {
 //			gridPane.add(team3, 0, 12);
 //			gridPane.add(team14, 0, 14);
 			gridPane.add(submit1_3, 0, 13);
-			gridPane.add(score1_5, 1, 12);
-			gridPane.add(score1_6, 1, 14);
+//			gridPane.add(score1_5, 1, 12);
+//			gridPane.add(score1_6, 1, 14);
 			
 			Button submit1_4 = new Button("Submit");
 			TextField score1_7 = new TextField("score...");
@@ -92,8 +137,8 @@ public class Main extends Application {
 //			gridPane.add(team4, 0, 17);
 //			gridPane.add(team13, 0, 19);
 			gridPane.add(submit1_4, 0, 18);
-			gridPane.add(score1_7, 1, 17);
-			gridPane.add(score1_8, 1, 19);
+//			gridPane.add(score1_7, 1, 17);
+//			gridPane.add(score1_8, 1, 19);
 			
 			Button submit1_5 = new Button("Submit");
 			TextField score1_9 = new TextField("score...");
@@ -103,8 +148,8 @@ public class Main extends Application {
 //			gridPane.add(team5, 0, 22);
 //			gridPane.add(team12, 0, 24);
 			gridPane.add(submit1_5, 0, 23);
-			gridPane.add(score1_9, 1, 22);
-			gridPane.add(score1_10, 1, 24);
+//			gridPane.add(score1_9, 1, 22);
+//			gridPane.add(score1_10, 1, 24);
 			
 			Button submit1_6 = new Button("Submit");
 			TextField score1_11 = new TextField("score...");
@@ -113,8 +158,8 @@ public class Main extends Application {
 //			gridPane.add(team6, 0, 27);
 //			gridPane.add(team11, 0, 29);
 			gridPane.add(submit1_6, 0, 28);
-			gridPane.add(score1_11, 1, 27);
-			gridPane.add(score1_12, 1, 29);
+//			gridPane.add(score1_11, 1, 27);
+//			gridPane.add(score1_12, 1, 29);
 			
 			Button submit1_7 = new Button("Submit");
 			TextField score1_13 = new TextField("score...");
@@ -124,8 +169,8 @@ public class Main extends Application {
 //			gridPane.add(team7, 0, 32);
 //			gridPane.add(team10, 0, 34);
 			gridPane.add(submit1_7, 0, 33);
-			gridPane.add(score1_13, 1, 32);
-			gridPane.add(score1_14, 1, 34);
+//			gridPane.add(score1_13, 1, 32);
+//			gridPane.add(score1_14, 1, 34);
 			
 			Button submit1_8 = new Button("Submit");
 			TextField score1_15 = new TextField("score...");
@@ -134,8 +179,8 @@ public class Main extends Application {
 //			gridPane.add(team8, 0, 37);
 //			gridPane.add(team9, 0, 39);
 			gridPane.add(submit1_8, 0, 38);
-			gridPane.add(score1_15, 1, 37);
-			gridPane.add(score1_16, 1, 39);
+//			gridPane.add(score1_15, 1, 37);
+//			gridPane.add(score1_16, 1, 39);
 	
 			////////////////////////////////////// END OF CODE FOR FIRST ROUND SETUP //////////////////////////////////
 			
@@ -178,14 +223,14 @@ public class Main extends Application {
 			gridPane.add(submit2_2, 2, 15);
 			gridPane.add(submit2_3, 2, 25);
 			gridPane.add(submit2_4, 2, 35);
-			gridPane.add(score2_1, 3, 3);
-			gridPane.add(score2_2, 3, 8);
-			gridPane.add(score2_3, 3, 13);
-			gridPane.add(score2_4, 3, 18);
-			gridPane.add(score2_5, 3, 23);
-			gridPane.add(score2_6, 3, 28);
-			gridPane.add(score2_7, 3, 33);
-			gridPane.add(score2_8, 3, 38);
+//			gridPane.add(score2_1, 3, 3);
+//			gridPane.add(score2_2, 3, 8);
+//			gridPane.add(score2_3, 3, 13);
+//			gridPane.add(score2_4, 3, 18);
+//			gridPane.add(score2_5, 3, 23);
+//			gridPane.add(score2_6, 3, 28);
+//			gridPane.add(score2_7, 3, 33);
+//			gridPane.add(score2_8, 3, 38);
 			gridPane.add(new Label(""), 3, 5);
 			
             //////////////////////////////////////END OF CODE FOR SECOND ROUND SETUP //////////////////////////////////
@@ -211,10 +256,10 @@ public class Main extends Application {
 			
 			gridPane.add(submit3_1, 4, 10);
 			gridPane.add(submit3_2, 4, 30);
-			gridPane.add(score3_1, 5, 5);
-			gridPane.add(score3_2, 5, 15);
-			gridPane.add(score3_3, 5, 25);
-			gridPane.add(score3_4, 5, 35);
+//			gridPane.add(score3_1, 5, 5);
+//			gridPane.add(score3_2, 5, 15);
+//			gridPane.add(score3_3, 5, 25);
+//			gridPane.add(score3_4, 5, 35);
 			
             //////////////////////////////////////END OF CODE FOR THIRD ROUND SETUP //////////////////////////////////
 			
@@ -230,8 +275,8 @@ public class Main extends Application {
 			submit4_1.setDisable(true);
 			
 			gridPane.add(submit4_1, 6, 20);
-			gridPane.add(score4_1, 7, 10);
-			gridPane.add(score4_2, 7, 30);
+//			gridPane.add(score4_1, 7, 10);
+//			gridPane.add(score4_2, 7, 30);
 			
             //////////////////////////////////////END OF CODE FOR FOURTH ROUND SETUP //////////////////////////////////
 			
@@ -261,10 +306,44 @@ public class Main extends Application {
 				seed++;
 			}
 			scnr.close();
+			
 			bracket = new Bracket(challengers);
+			
+			numSubmit = challengers.size() - 1;
+			numTextFields = (challengers.size() * 2) - 2;
+			numTBDLabels = (challengers.size()) - 2;
+			numRounds = (int) (Math.log(challengers.size()) / Math.log(2));
+			
 			teamLabels = new ArrayList<Label>();
 			for(int i=0; i<challengers.size(); i++) {
-				teamLabels.add(new Label(challengers.get(i).getName()));
+				teamLabels.add(new Label(bracket.getActiveChallengers().get(i).getName()));
+			}
+			for(int i=0; i<teamLabels.size()/2 - 1; i+=2) {
+				bracket.getMatchups().add(new Matchup(bracket.getAllChallengers()[i], bracket.getAllChallengers()[i+1]));
+			}
+			
+			teamScores = new ArrayList<ArrayList<TextField>>();
+			for(int i=numRounds; i>0; i--) {
+				teamScores.add(new ArrayList<TextField>((int) Math.pow(2, i)));
+				for(int j=0; j<Math.pow(2, i); j++) {
+					teamScores.get(teamScores.size()-1).add(new TextField("scores..."));
+				}
+			}
+			
+			submitButtons = new ArrayList<ArrayList<Button>>();
+			for(int i=numRounds; i>0; i--) {
+				submitButtons.add(new ArrayList<Button>((int) ((Math.pow(2, i))/2)));
+				for(int j=0; j<(Math.pow(2, i)/2); j++) {
+					submitButtons.get(submitButtons.size()-1).add(new Button("Submit"));
+				}
+			}
+			
+			TBDLabels = new ArrayList<ArrayList<Label>>();
+			for(int i=numRounds-1; i>0; i--) {
+				TBDLabels.add(new ArrayList<Label>((int) (Math.pow(2, i))/2));
+				for(int j=0; j<(Math.pow(2, i)/2); j++) {
+					TBDLabels.get(TBDLabels.size()-1).add(new Label("TBD"));
+				}
 			}
 		} catch(FileNotFoundException e) {
 			System.out.println("ERROR: File not found.");
