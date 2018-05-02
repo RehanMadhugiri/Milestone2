6a7f10c9534889b9a77e503f84f976964dcb7efe
Andy Waldron <awaldron2@wisc.edu>
2018-05-01 20:52:52 -0500

    More comments.

--- src/application/Challenger.java
+++ src/application/Challenger.java
@@ -17,7 +17,7 @@ public class Challenger {
 	
 	private String name; // Name of challenger.
 	private int score; // Score of challenger at a certain time.
-	private Label label;
+	private Label label; // Label in the JavaFX stage of the team.
 	
 	/**
 	 * Challenger constructor which sets the challenger's name to whatever is passed in the
@@ -41,36 +41,52 @@ public class Challenger {
 		return score;
 	}
 	
-
+	/**
+	 * This method returns the label in the JavaFX of the challenger.
+	 * 
+	 * @param No parameters.
+	 * @return The label of the challenger.
+	 */
+	public Label getLabel() {
+		return label;
+	}
+	
 	/**
 	 * This method sets the score of the challenger to whatever int is passed in as an argument.
 	 * 
 	 * @param newScore The score of the challenger.
 	 * @return Nothing returned.
 	 */
-
-	public Label getLabel() {
-		return label;
-	}
-	
-
 	public void setScore(int newScore) {
 		score = newScore;
 	}
 	
 	/**
-	 * This method returns the name of the 
+	 * This method returns the name of the challenger as a string.
 	 * 
-	 * @return
+	 * @param No parameters.
+	 * @return The name of the challenger as a string.
 	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * This method sets the name of the challenger to whatever is passed in as an argument.
+	 * 
+	 * @param newName The name of the challenger.
+	 * @return Nothing returned.
+	 */
 	public void setName(String newName) {
 		name = newName;
 	}
 	
+	/**
+	 * This method returns the name of the challenger as a string.
+	 * 
+	 * @param No parameters.
+	 * @return The name of the challenger as a string.
+	 */
 	public String toString() {
 		return name;
 	}
